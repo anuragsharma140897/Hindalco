b5f2a2f13f1cdd7df314a4e9a818c8d0109f9819
surajheadsup <90753070+surajheadsup@users.noreply.github.com>
2024-05-24 13:35:00 +0530

    init deployment

--- src/pages/fonts.ts
+++ src/pages/fonts.ts
@@ -2,7 +2,7 @@ import { Inter, Lexend_Deca } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
-export const lexendDeca = Lexend_Deca({
+export const LexendDeca = Lexend_Deca({
   subsets: ["latin"],
   variable: "--font-lexend",
 });
