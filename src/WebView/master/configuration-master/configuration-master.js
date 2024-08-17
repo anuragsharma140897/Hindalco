8945b74a124fa283e745c6d342892a8164fcc223
Shariq Ansari <ansari.shariq.badshah@gmail.com>
2024-08-17 23:00:17 +0530

    pushing congig

--- src/WebView/master/configuration-master/configuration-master.js
+++ src/WebView/master/configuration-master/configuration-master.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import PageHeader from '../../../shared/page-header'
+import AddConfigurationMaster from '../../../Form/master/configuration-master/add-configuration-master'
+import { useModal } from '../../../shared/modal-views/use-modal';
+import { getGeneralMasterColumns } from '../general-master/general-column';
+import { useColumn } from '../../../Hooks/use-column';
+
 
 function ConfigurationMaster() {
+  const { openModal, closeModal } = useModal();
+  const columns = useMemo(() => getGeneralMasterColumns({ openModal, closeModal }))
+  const { visibleColumns } = useColumn(columns);
   return (
-    <div>configuration-master</div>
+    <div>
+            <PageHeader btnText={'Add Configuration'} children={<AddConfigurationMaster closeModal={closeModal} />} title={'Add Configuration'} customSize={400} />
+
+    </div>
   )
 }
 
