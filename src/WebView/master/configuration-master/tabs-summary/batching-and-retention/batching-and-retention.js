0776a54a0431c4fd7aa6f09010dae8bbc3f35698
Aman Khan <amanacer58500@gmail.com>
2024-08-19 15:30:33 +0530

    merge with shariq

--- src/WebView/master/configuration-master/tabs-summary/batching-and-retention/batching-and-retention.js
+++ src/WebView/master/configuration-master/tabs-summary/batching-and-retention/batching-and-retention.js
@@ -4,7 +4,7 @@ import CustomInput from '../../../../../Component/ui/form/input/custom-input'
 function BatchingAndRetention() {
   return (
     <div className='py-5 bg-white'>
-    <div >
+    <div>
         <div>
             <div className='font-bold text-base bg-lightpink p-2'>Batching</div>
             <div className='mt-5 grid grid-cols-3 gap-x-4 '>
@@ -20,8 +20,6 @@ function BatchingAndRetention() {
                 <CustomInput important={true} name="value" label="Events Retention Time" />
             </div>
         </div>
-  
-
     </div>
 
 </div>
