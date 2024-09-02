8b87b0d7c819a74353b9b948eb035e4f2e61d918
surajheadsup <90753070+surajheadsup@users.noreply.github.com>
2024-09-02 16:25:55 +0530

    code pushed before pull

--- src/Constant/Api/Api.js
+++ src/Constant/Api/Api.js
@@ -1,7 +1,7 @@
 
 // export const IP = '137.184.74.218'
 export const IP = '192.168.0.104:9090'
-export const MQTTIP = 'ws://192.168.0cle.104:9001'
+export const MQTTIP = 'ws://192.168.0.104:9001'
 export const BaseUrl = `http://${IP}/auth/`
 export const UserMicroService = `http://${IP}/user/api/`
 export const HelperMicroService = `http://${IP}/helper/api/`
